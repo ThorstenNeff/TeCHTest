4182c98bb562bb338c8f3c53c636aefe0a87943e
Thorsten Neff <thorstenneff@Mac-Pro-von-Thorsten.local>
2024-09-20 09:07:03 +0200

    Pass replica tokens as parameter

--- scripts/run2.sh
+++ scripts/run2.sh
@@ -2,8 +2,8 @@ set -x
 export INPUT_FILE=$1;
 export EXP_DIR=$2;
 export SUBJECT_NAME=$(basename $1 | cut -d"." -f1);
-export REPLICATE_API_TOKEN=""; # your replicate token for BLIP API
-export CUDA_HOME=/usr/local/cuda-12.2/ #/your/cuda/home/dir;
+export REPLICATE_API_TOKEN=$4;
+export CUDA_HOME=/usr/local/cuda-12.2/;
 export PYOPENGL_PLATFORM=osmesa
 export MESA_GL_VERSION_OVERRIDE=4.1
 export PYTHONPATH=$PYTHONPATH:$(pwd);

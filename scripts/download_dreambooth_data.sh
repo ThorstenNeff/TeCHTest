686f667b502e030c99dc099f1526a886e34e14a6
Thorsten Neff <thorstenneff@Mac-Pro-von-Thorsten.local>
2024-09-20 09:12:02 +0200

    Fetch models from server

--- scripts/download_dreambooth_data.sh
+++ scripts/download_dreambooth_data.sh
@@ -3,12 +3,10 @@ mkdir -p data/dreambooth_data
 
 # SD v1-5 LDM checkpoint
 echo -e "\nDownloading stable diffusion v1.5..."
-cp /content/drive/MyDrive/TechConda/dreambooth_data/v1-5-pruned.ckpt /content/TeCH/data/dreambooth_data/v1-5-pruned.ckpt
-
+wget 'https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned.ckpt' -O data/dreambooth_data/v1-5-pruned.ckpt
 
 # ECON
 echo -e "\nDownloading dreambooth background images and regularization images..."
-cp /content/drive/MyDrive/TechConda/dreambooth_data/dreambooth_data.zip /content/TeCH/data/dreambooth_data/dreambooth_data.zip
-cd /content/TeCH/data/dreambooth_data && unzip dreambooth_data.zip
+wget 'https://www.dropbox.com/scl/fi/ucj961vt90hix12up2nyv/dreambooth_data.zip?rlkey=w1frc8hzkjskmnesokextp84r&dl=0' -O 'data/dreambooth_data/dreambooth_data.zip' --no-check-certificate --continue
+cd data/dreambooth_data && unzip dreambooth_data.zip
 rm -f dreambooth_data.zip
-
